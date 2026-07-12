4462b4ab1b760a42e5cb5923853df864c789edec
ch0pe <acbsur1@gmail.com>
2026-07-12 14:55:27 +0200

    Add static array class

---
+++ src/rbe/core/static_array.hpp
@@ -0,0 +1,126 @@
+/************************************************************************
+ * Copyright (c) 2026 Alvaro Cabrera Barrio
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ ************************************************************************/
+/**
+ * @file static_array.hpp
+ * @date 07/07/2026
+ * @brief Short description
+ *
+ * Longer description
+ */
+
+#pragma once
+
+// --- Includes ---
+
+// --- Dependencies ---
+
+// --- External dependencies ---
+
+// --- STD ---
+#include <initializer_list>
+#include <meta>
+#include <ranges>
+#include <span>
+
+// --- System ---
+
+namespace rbe {
+
+namespace detail {
+
+// TODO: move from public API to detail namespace
+template<typename R, typename T>
+concept compatible_range = std::ranges::input_range<R> and std::convertible_to<std::ranges::range_reference_t<R>, T>;
+
+} // namespace detail
+
+
+template<typename T>
+  requires std::same_as<std::remove_cvref_t<T>, T>
+class static_array {
+public:
+  using underlying_span_type   = std::span<T const>;
+  using element_type           = underlying_span_type::element_type;
+  using value_type             = underlying_span_type::value_type;
+  using size_type              = underlying_span_type::size_type;
+  using difference_type        = underlying_span_type::difference_type;
+  using const_pointer          = underlying_span_type::const_pointer;
+  using const_reference        = underlying_span_type::const_reference;
+  using const_iterator         = underlying_span_type::const_iterator;
+  using const_reverse_iterator = underlying_span_type::const_reverse_iterator;
+
+  consteval static_array() = default;
+
+  template<detail::compatible_range<T> R>
+  consteval static_array(std::from_range_t /**/, R&& range) :
+    data_(std::define_static_array(std::forward<R>(range))) { }
+
+  template<std::input_iterator InputIt>
+  consteval static_array(InputIt first, InputIt last) : data_(std::define_static_array(first, last)) { }
+
+  consteval static_array(std::initializer_list<T> init) : data_(std::define_static_array(init)) { }
+
+  [[nodiscard]] constexpr const_iterator begin() const { return data_.begin(); }
+
+  [[nodiscard]] constexpr const_iterator end() const { return data_.end(); }
+
+  [[nodiscard]] constexpr const_reverse_iterator rbegin() const { return data_.rbegin(); }
+
+  [[nodiscard]] constexpr const_reverse_iterator rend() const { return data_.rend(); }
+
+  [[nodiscard]] constexpr const_iterator cbegin() const { return data_.cbegin(); }
+
+  [[nodiscard]] constexpr const_iterator cend() const { return data_.cend(); }
+
+  [[nodiscard]] constexpr const_reverse_iterator crbegin() const { return data_.crbegin(); }
+
+  [[nodiscard]] constexpr const_reverse_iterator crend() const { return data_.crend(); }
+
+  [[nodiscard]] constexpr size_type size() const { return data_.size(); }
+
+  [[nodiscard]] constexpr size_type size_bytes() const { return data_.size_bytes(); }
+
+  [[nodiscard]] constexpr bool empty() const { return data_.empty(); }
+
+  [[nodiscard]] constexpr const_reference operator[](std::size_t index) const { return data_[index]; }
+
+  [[nodiscard]] constexpr const_reference at(std::size_t index) const { return data_.at(index); }
+
+  [[nodiscard]] constexpr const_reference front() const { return data_.front(); }
+
+  [[nodiscard]] constexpr const_reference back() const { return data_.back(); }
+
+  [[nodiscard]] constexpr const_pointer data() const { return data_.data(); }
+
+  [[nodiscard]] constexpr static_array first(size_type count) const {
+    return std::define_static_array(data_.first(count));
+  }
+
+  [[nodiscard]] constexpr static_array last(size_type offset) const {
+    return std::define_static_array(data_.last(offset));
+  }
+
+  [[nodiscard]] constexpr static_array subspan(size_type offset, size_type count = std::dynamic_extent) const {
+    return std::define_static_array(data_.subspan(offset, count));
+  }
+
+  [[nodiscard]] constexpr bool operator==(static_array const& other) const {
+    return std::ranges::equal(data_, other.data_);
+  }
+
+private:
+  std::span<T const> data_;
+};
+
+template<std::ranges::range R>
+static_array(std::from_range_t, R&&) -> static_array<std::ranges::range_value_t<R>>;
+
+template<std::input_iterator InputIt>
+static_array(InputIt, InputIt) -> static_array<std::iter_value_t<InputIt>>;
+
+template<typename T>
+static_array(std::initializer_list<T>) -> static_array<T>;
+
+} // namespace rbe
---
+++ tests/runtime/test_static_array.cpp
@@ -0,0 +1,273 @@
+/************************************************************************
+ * Copyright (c) 2026 Alvaro Cabrera Barrio
+ * This code is licensed under MIT license (see LICENSE.txt for details)
+ ************************************************************************/
+/**
+ * @file test_static_array.cpp
+ * @date 08/07/2026
+ * @brief Short description
+ *
+ * Longer description
+ */
+
+// --- Includes ---
+#include <rbe/core/static_array.hpp>
+
+// --- Dependencies ---
+
+// --- External dependencies ---
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+// --- STD ---
+#include <ranges>
+
+// --- System ---
+
+struct SomeData {
+  int a;
+  char b;
+};
+
+struct SomeNumbers {
+  int a;
+  int b;
+  int c;
+  int d;
+};
+
+// --- Compile time tests ---
+
+consteval bool construct_default() {
+  rbe::static_array<int> arr;
+  static_assert(std::is_same_v<decltype(arr), rbe::static_array<int>>);
+  return arr.size() == 0 and arr.empty();
+}
+
+consteval bool construct_with_vec() {
+  std::vector<int> vec {1, 2, 3, 4, 5};
+  rbe::static_array<int> arr {std::from_range, vec};
+  static_assert(std::is_same_v<decltype(arr), rbe::static_array<int>>);
+  return arr.size() == 5 and arr[0] == 1 and arr[1] == 2 and arr[2] == 3 and arr[3] == 4 and arr[4] == 5;
+}
+
+consteval bool construct_with_array() {
+  std::array<int, 5> arr_data {1, 2, 3, 4, 5};
+  rbe::static_array arr {std::from_range, arr_data};
+  static_assert(std::is_same_v<decltype(arr), rbe::static_array<int>>);
+  return arr.size() == 5 and arr[0] == 1 and arr[1] == 2 and arr[2] == 3 and arr[3] == 4 and arr[4] == 5;
+}
+
+consteval bool initializer_list() {
+  rbe::static_array arr {1, 2, 3, 4, 5};
+
+  return arr.size() == 5 and arr[0] == 1 and arr[1] == 2 and arr[2] == 3 and arr[3] == 4 and arr[4] == 5;
+}
+
+consteval bool initializer_list_deduction() {
+  rbe::static_array arr {
+    SomeData {.a = 1, .b = 'a'},
+    SomeData {.a = 2, .b = 'b'},
+  };
+
+  static_assert(std::is_same_v<decltype(arr), rbe::static_array<SomeData>>);
+  return arr.size() == 2 and arr[0].a == 1 and arr[0].b == 'a' and arr[1].a == 2 and arr[1].b == 'b';
+}
+
+consteval bool static_array_random_access() {
+  rbe::static_array arr {
+    SomeData {.a = 1, .b = 'a'}, SomeData {.a = 2, .b = 'b'}, SomeData {.a = 3, .b = 'c'}, SomeData {.a = 4, .b = 'd'},
+    SomeData {.a = 5, .b = 'e'}, SomeData {.a = 6, .b = 'f'}, SomeData {.a = 7, .b = 'g'},
+  };
+
+  bool result = arr.at(0).a == 1 and arr.at(1).a == 2 and arr.at(2).a == 3 and arr.at(3).a == 4 and arr.at(4).a == 5 and
+                arr.at(5).a == 6 and arr.at(6).a == 7;
+
+  result &= arr[0].a == 1 and arr[1].a == 2 and arr[2].a == 3 and arr[3].a == 4 and arr[4].a == 5 and arr[5].a == 6 and
+            arr[6].a == 7 and result;
+
+  result &= arr.front().a == 1 and arr.back().a == 7 and result;
+
+  return result;
+}
+
+consteval bool array_iteration() {
+  rbe::static_array arr {
+    SomeData {.a = 1, .b = 'a'}, SomeData {.a = 2, .b = 'b'}, SomeData {.a = 3, .b = 'c'}, SomeData {.a = 4, .b = 'd'},
+    SomeData {.a = 5, .b = 'e'}, SomeData {.a = 6, .b = 'f'}, SomeData {.a = 7, .b = 'g'},
+  };
+
+  std::vector<int> expected = {1, 2, 3, 4, 5, 6, 7};
+
+  for (auto const& [idx, item]: arr | std::views::enumerate) {
+    if (item.a != idx + 1) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+consteval bool array_reverse_iteration() {
+  rbe::static_array arr = {
+    SomeData {.a = 1, .b = 'a'}, SomeData {.a = 2, .b = 'b'}, SomeData {.a = 3, .b = 'c'}, SomeData {.a = 4, .b = 'd'},
+    SomeData {.a = 5, .b = 'e'}, SomeData {.a = 6, .b = 'f'}, SomeData {.a = 7, .b = 'g'},
+  };
+
+  for (auto const& [idx, item]: arr | std::views::reverse | std::views::enumerate) {
+    if (static_cast<std::size_t>(item.a) != arr.size() - static_cast<std::size_t>(idx)) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+consteval bool template_for_iteration() {
+  SomeNumbers data {
+    .a = 0,
+    .b = 1,
+    .c = 2,
+    .d = 3,
+  };
+
+  int counter = 0;
+
+  static constexpr auto ctx     = std::meta::access_context::current();
+  static constexpr auto members = nonstatic_data_members_of(^^SomeNumbers, ctx) | std::ranges::to<rbe::static_array>();
+  template for (constexpr auto member: members) {
+    if (data.[:member:] != counter++) {
+      return false;
+    }
+  }
+  return true;
+}
+
+static_assert(construct_default(), "construct_default test failed");
+static_assert(construct_with_vec(), "construct_with_vec test failed");
+static_assert(construct_with_array(), "construct_with_array test failed");
+static_assert(initializer_list(), "initializer_list test failed");
+static_assert(initializer_list_deduction(), "initializer_list_deduction test failed");
+static_assert(static_array_random_access(), "static_array_random_access test failed");
+static_assert(array_iteration(), "array_iteration test failed");
+static_assert(array_reverse_iteration(), "array_reverse_iteration test failed");
+static_assert(template_for_iteration(), "template_for_iteration test failed");
+
+// --- Runtime tests ---
+
+TEST_SUITE_BEGIN("static_array");
+
+TEST_CASE("Default construction") {
+  static constexpr rbe::static_array<int> arr;
+  CHECK(arr.size() == 0);
+  CHECK(arr.empty());
+}
+
+TEST_CASE("Construct with vector") {
+  constexpr std::array<int, 5> vec_data {1, 2, 3, 4, 5};
+  static constexpr rbe::static_array<int> arr {std::from_range, vec_data};
+  CHECK(arr.size() == 5);
+  CHECK(arr[0] == 1);
+  CHECK(arr[1] == 2);
+  CHECK(arr[2] == 3);
+  CHECK(arr[3] == 4);
+  CHECK(arr[4] == 5);
+}
+
+TEST_CASE("Construct with array") {
+  constexpr std::array<int, 5> arr_data {1, 2, 3, 4, 5};
+  static constexpr rbe::static_array arr {std::from_range, arr_data};
+  CHECK(arr.size() == 5);
+  CHECK(arr[0] == 1);
+  CHECK(arr[1] == 2);
+  CHECK(arr[2] == 3);
+  CHECK(arr[3] == 4);
+  CHECK(arr[4] == 5);
+}
+
+TEST_CASE("Initializer list") {
+  static constexpr rbe::static_array arr {1, 2, 3, 4, 5};
+  CHECK(arr.size() == 5);
+  CHECK(arr[0] == 1);
+  CHECK(arr[1] == 2);
+  CHECK(arr[2] == 3);
+  CHECK(arr[3] == 4);
+  CHECK(arr[4] == 5);
+}
+
+TEST_CASE("Initializer list deduction") {
+  static constexpr rbe::static_array arr {
+    SomeData {.a = 1, .b = 'a'},
+    SomeData {.a = 2, .b = 'b'},
+  };
+  CHECK(arr.size() == 2);
+  CHECK(arr[0].a == 1);
+  CHECK(arr[0].b == 'a');
+  CHECK(arr[1].a == 2);
+  CHECK(arr[1].b == 'b');
+}
+
+TEST_CASE("Static array random access") {
+  static constexpr rbe::static_array arr {
+    SomeData {.a = 1, .b = 'a'}, SomeData {.a = 2, .b = 'b'}, SomeData {.a = 3, .b = 'c'}, SomeData {.a = 4, .b = 'd'},
+    SomeData {.a = 5, .b = 'e'}, SomeData {.a = 6, .b = 'f'}, SomeData {.a = 7, .b = 'g'},
+  };
+
+  CHECK(arr.at(0).a == 1);
+  CHECK(arr.at(1).a == 2);
+  CHECK(arr.at(2).a == 3);
+  CHECK(arr.at(3).a == 4);
+  CHECK(arr.at(4).a == 5);
+  CHECK(arr.at(5).a == 6);
+  CHECK(arr.at(6).a == 7);
+
+  CHECK(arr[0].a == 1);
+  CHECK(arr[1].a == 2);
+  CHECK(arr[2].a == 3);
+  CHECK(arr[3].a == 4);
+  CHECK(arr[4].a == 5);
+  CHECK(arr[5].a == 6);
+  CHECK(arr[6].a == 7);
+
+  CHECK(arr.front().a == 1);
+  CHECK(arr.back().a == 7);
+}
+
+TEST_CASE("Array iteration") {
+  static constexpr rbe::static_array arr {
+    SomeData {.a = 1, .b = 'a'}, SomeData {.a = 2, .b = 'b'}, SomeData {.a = 3, .b = 'c'}, SomeData {.a = 4, .b = 'd'},
+    SomeData {.a = 5, .b = 'e'}, SomeData {.a = 6, .b = 'f'}, SomeData {.a = 7, .b = 'g'},
+  };
+
+  for (auto const& [idx, item]: arr | std::views::enumerate) {
+    CHECK(item.a == static_cast<int>(idx) + 1);
+  }
+}
+
+TEST_CASE("Array reverse iteration") {
+  static constexpr rbe::static_array arr {
+    SomeData {.a = 1, .b = 'a'}, SomeData {.a = 2, .b = 'b'}, SomeData {.a = 3, .b = 'c'}, SomeData {.a = 4, .b = 'd'},
+    SomeData {.a = 5, .b = 'e'}, SomeData {.a = 6, .b = 'f'}, SomeData {.a = 7, .b = 'g'},
+  };
+
+  for (auto const& [idx, item]: arr | std::views::reverse | std::views::enumerate) {
+    CHECK(static_cast<std::size_t>(item.a) == arr.size() - static_cast<std::size_t>(idx));
+  }
+}
+
+TEST_CASE("Template for iteration") {
+  static constexpr SomeNumbers data {
+    .a = 0,
+    .b = 1,
+    .c = 2,
+    .d = 3,
+  };
+
+  constexpr static auto ctx     = std::meta::access_context::current();
+  constexpr static auto members = nonstatic_data_members_of(^^SomeNumbers, ctx) | std::ranges::to<rbe::static_array>();
+
+  int counter = 0;
+  template for (constexpr auto member: members) { CHECK(data.[:member:] == counter++); }
+}
+
+TEST_SUITE_END();
