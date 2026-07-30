0cb29e6a3551e1facf161f9aa63b8ec84f947bfe
ch0pex <acbsur1@gmail.com>
2026-07-31 01:30:33 +0200

    Refactor serialize function to user custom serder

--- src/rbe/srl/serialize.hpp
+++ src/rbe/srl/serialize.hpp
@@ -17,6 +17,8 @@
 #include <rbe/core/memory_layout.hpp>
 #include <rbe/detail/memcpy_constexpr.hpp>
 
+#include "rbe/core/serder.hpp"
+
 // --- Dependencies ---
 
 // --- External dependencies ---
@@ -29,40 +31,19 @@
 namespace rbe {
 
 /**
- * @brief Serializes a trivially serializable type into a buffer.
+ * @brief Serializes a wirable type into a buffer.
  *
- * This function serializes the given value of type `T` into the provided buffer. The type `T` must be trivially
- * serializable, meaning that its struct layout matches its wire layout, allowing for direct memory copying.
+ * This function serializes the given value of type `T` into the provided buffer. The type `T` must be wirable
  *
  * @tparam T The type of the value to serialize. Must be trivially serializable.
- * @param buffer A span of bytes where the serialized data will be stored. The size of the buffer must be at least
+ * @param out A span of bytes where the serialized data will be stored. The size of the buffer must be at least
  * sizeof(T).
  * @param value The value to serialize.
  * @return The number of bytes written to the buffer, which is equal to sizeof(T).
  */
-template<trivially_serializable T>
-constexpr auto serialize(std::span<std::byte> out, T const& value) -> std::size_t {
-  detail::memcpy_constexpr(out, value);
-  return sizeof(T);
-}
-
-// NOTE: this is a baseline implementation of the serialize function for introspectable types.
-// It serializes each member of the struct and perform byte swapping if needed.
-// future implementations could detect chunks of the struct that can be memcpy'ed directly to the buffer
-// to reduce the number of memcpy calls and potentially improve performance (?).
-template<introspectable T>
-constexpr auto serialize(std::span<std::byte> out, T const& value) -> std::size_t {
-  using std::ranges::to;
-
-  static constexpr auto wire    = get_wire_layout<T>();
-  static constexpr auto members = detail::nsdm(^^T);
-
-  template for (constexpr auto [layout, member]: std::views::zip(wire.members, members) | to<static_array>()) {
-    auto dst = out.subspan<layout.offset.bytes, layout.size>();
-    detail::memcpy_constexpr(dst, endian::native_to<layout.endianness>(value.[:member:]));
-  }
-
-  return wire.size;
+template<wirable T>
+constexpr auto serialize(std::span<std::byte> const out, T const& value) -> std::size_t {
+  return serder<T>::serialize(out, value);
 }
 
 } // namespace rbe
